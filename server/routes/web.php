3a5f6fdb8f9c10d09bd7ce22f89452aa52ddc8d6
Thilina Ruwan <106557565+thilinaruwan112@users.noreply.github.com>
2024-09-08 11:21:42 +0530

    add cros site requests

--- server/routes/web.php
+++ server/routes/web.php
@@ -91,6 +91,10 @@ if ($_SERVER['HTTP_HOST'] === 'localhost') {
 if ($uri !== '/') {
     header('Content-Type: application/json');
 }
+// Set CORS headers for all responses
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 // Debugging
 error_log("Method: $method");
