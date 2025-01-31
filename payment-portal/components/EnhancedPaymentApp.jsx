6303aeb78aded2b9d5ab0073bf4433ae075e1384
Thilina Ruwan <106557565+thilinaruwan112@users.noreply.github.com>
2025-02-01 03:00:25 +0530

    update server ftp

--- payment-portal/components/EnhancedPaymentApp.jsx
+++ payment-portal/components/EnhancedPaymentApp.jsx
@@ -92,7 +92,7 @@ export default function EnhancedPaymentApp() {
       formDataToSend.append("slip", formData.slip); // File upload
 
       const response = await fetch(
-        "https://qa-api.pharmacollege.lk/payment-portal-requests",
+        "http://localhost/pharma-college-project/server/payment-portal-requests",
         {
           method: "POST",
           body: formDataToSend, // Sending as FormData
