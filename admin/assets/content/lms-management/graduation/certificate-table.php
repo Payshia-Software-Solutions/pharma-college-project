96d400e31c1f8b6d6dbe176335a6f6156229af68
dewRuwan112 <106557565+thilinaruwan112@users.noreply.github.com>
2025-06-07 00:28:23 +0530

    add model for certificate print

--- admin/assets/content/lms-management/graduation/certificate-table.php
+++ admin/assets/content/lms-management/graduation/certificate-table.php
@@ -14,7 +14,77 @@ $UserLevel = $_POST['UserLevel'];
 
 if (strtolower($UserLevel) != 'admin') die('Access denied');
 
-$courseCode = $_POST['courseCode'] ?? null;
-$showSession = $_POST['showSession'] ?? null;
+$courseCode = isset($_POST['courseCode']) ? $_POST['courseCode'] : null;
+$showSession = isset($_POST['showSession']) ? $_POST['showSession'] : null;
 
-$packageBookings = $client->request('GET', $_ENV['SERVER_URL'] . '/convocation-registrations?courseCode=' . $courseCode . '&viewSession=' . $showSession)->toArray();
+$packageBookings = [];
+$packageBookingsByCourse = [];
+$packageBookingsBySession = [];
+
+if (isset($courseCode) && isset($showSession)) {
+    $packageBookings = $client->request(
+        'GET',
+        $_ENV['SERVER_URL'] . '/convocation-registrations?courseCode=' . $courseCode . '&viewSession=' . $showSession
+    )->toArray();
+}
+
+if (isset($courseCode)) {
+    $packageBookings = $client->request(
+        'GET',
+        $_ENV['SERVER_URL'] . '/convocation-registrations?courseCode=' . $courseCode
+    )->toArray();
+}
+
+if (isset($showSession)) {
+    $packageBookings = $client->request(
+        'GET',
+        $_ENV['SERVER_URL'] . '/convocation-registrations?viewSession=' . $showSession
+    )->toArray();
+}
+?>
+
+<div class="card">
+    <div class="card-body">
+        <h6 class="table-title m-0 mb-2">Course - <?= $courseCode ?> | Session - <?= $showSession ?></h4>
+            <div class="table-responsive">
+                <table class="table table-striped table-hover" id="certificate-table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Reference #</th>
+                            <th scope="col">Student Number</th>
+                            <th scope="col">Session</th>
+                            <th scope="col">Paid</th>
+                            <th scope="col">Registration Status</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($packageBookings as $booking) : ?>
+                            <tr>
+                                <td><?= $booking['registration_id'] ?></td>
+                                <td><?= $booking['student_number'] ?></td>
+                                <td><?= $booking['session'] ?></td>
+                                <td><?= $booking['payment_amount'] ?></td>
+                                <td><?= $booking['registration_status'] ?></td>
+                                <td><button class="btn btn-dark btn-sm" type="button" onclick="OpenCertificateModel('<?= $booking['registration_id'] ?>')">View</button></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+    </div>
+</div>
+
+<script>
+    $('#certificate-table').dataTable({
+        dom: 'Bfrtip',
+        pageLength: 50,
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print',
+            // 'colvis'
+        ],
+        order: [
+            [0, 'asc']
+        ]
+    })
+</script>
