f0a9d55e5d8d3380db5229b43badf95b6c0a235d
thilinaruwan11 <ict20838@fot.sjp.ac.lk>
2025-06-15 19:53:12 +0530

    Update UserCertificatePrintStatusControllerNew.php

--- server/controllers/UserCertificatePrintStatusControllerNew.php
+++ server/controllers/UserCertificatePrintStatusControllerNew.php
@@ -55,16 +55,17 @@ class UserCertificatePrintStatusControllerNew
 
         if ($parentCourseCode == "1") {
             // Update the convocation registration with the new certificate_id
-            $this->convocationRegistrationModel->updateCertificatePrintStatus("Generated", $reference_number, $certificateId,);
+            $certificateIdStatus = $this->convocationRegistrationModel->updateCertificatePrintStatus("Generated", $reference_number, $certificateId,);
         } else if ($parentCourseCode == "2") {
             // Update the convocation registration with the new certificate_id
-            $this->convocationRegistrationModel->updateAdvancedCertificatePrintStatus("Generated", $reference_number, $certificateId,);
+            $certificateIdStatus = $this->convocationRegistrationModel->updateAdvancedCertificatePrintStatus("Generated", $reference_number, $certificateId,);
         }
 
         http_response_code(201);
         echo json_encode([
             "message"        => "Status created successfully",
-            "certificate_id" => $certificateId
+            "certificate_id" => $certificateId,
+            "certificate_print_status" => $certificateIdStatus ? "Updated" : "Failed to update convocation registration",
         ]);
     }
 
