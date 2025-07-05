7f3d716088a6f60a5c09e7315b0022b4b762f399
thilinaruwan11 <ict20838@fot.sjp.ac.lk>
2025-07-05 23:29:59 +0530

    Update print-all-certificates-course.php

--- admin/assets/content/lms-management/certification/print-view/print-all-certificates-course.php
+++ admin/assets/content/lms-management/certification/print-view/print-all-certificates-course.php
@@ -23,6 +23,7 @@ require_once('../../../../../vendor/phpqrcode/qrlib.php');
 
 $courseCode = isset($_GET['courseCode']) ? $_GET['courseCode'] : null;
 $showSession = isset($_GET['showSession']) ? $_GET['showSession'] : null;
+$tableMode = isset($_GET['tableMode']) ? $_GET['tableMode'] : 1;
 
 if ($courseCode == 1) {
     $courseName = "Certificate Course in Pharmacy Practice";
@@ -278,91 +279,113 @@ if (isset($courseCode) && isset($showSession)) {
         }
     }
 </style>
-<?php
-$count = 1;
-foreach ($packageBookings as $booking) {
-    // break;
-    $s_user_name = $booking['student_number'];
-    // $CourseCode = 1;
 
-    $batchStudents =  GetLmsStudents();
-    $studentDetailsArray = $batchStudents[$s_user_name];
-
-    $certificateId = ($courseCode == 1) ? $booking['certificate_id'] : $booking['advanced_id'];
-    $certificateInfo = CertificatePrintStatusByCertificateId($certificateId);
-    // var_dump($certificateInfo);
-
-    $PrintDate = date("Y-m-d H:i:s");
-    // $certificateEntryResult = EnterCertificateEntry($printDate, 1, $loggedUser, 'Workshop-Certificate', $s_user_name, $CourseCode);
-    // var_dump($certificateEntryResult);
-
-    // Include the qrlib file 
-    if (empty($certificateId)) {
-        $certificateInfo = CertificatePrintStatusByParentCourse($courseCode, 'Certificate', $s_user_name);
-        // var_dump($certificateInfo);
-        $certificateId = $certificateInfo[$s_user_name]['certificate_id'];
-        $batchCode = $certificateInfo[$s_user_name]['course_code'];
-    } else {
-        $batchCode = $certificateInfo[0]['course_code'];
-    }
-
-    $text = "https://pharmacollege.lk/result-view.php?CourseCode=" . $batchCode . "&LoggedUser=" . $s_user_name;
-    $ecc = 'L';
-    $pixel_Size = 10;
-    $frame_Size = 0;
-
-    // Generate the QR code image
-    ob_start();
-    QRcode::png($text, null, QR_ECLEVEL_L, 10, 0);
-    $image_data = ob_get_contents();
-    ob_end_clean();
-?>
-
-
-    <div class="certificate-container d-none">
-        <div class="certificate">
-            <h1 class="certificate-title">CERTIFICATE OF COMPLETION</h1>
-
-            <p class="awarded-to">This certificate is awarded to</p>
-            <div class="recipient-line"><?= $studentDetailsArray['name_on_certificate'] ?></div>
-
-            <div class="recognition-text">
-                in recognition of the<br>
-                successful completion of the
-            </div>
-            <div class="course-line"><?= $courseName ?></div>
-
-            <p class="offered-by">offered by</p>
-            <div class="institution">Ceylon Pharma College</div>
-            <div class="ceremony-details">
-                The certificate award ceremony was held at the<br>
-                BMICH, Colombo, Sri Lanka.
-            </div>
-        </div>
-
-        <img class="qr-code" src="data:image/png;base64,<?= base64_encode($image_data) ?>">
-        <?php
-        if ($courseCode == 2) { ?>
-            <img class="sign" src="sign.png" alt="">
-            <div>
-            </div>
-            <p class="sign-dot">...................................................</p>
-            <p class="director">Director</p>
-        <?php } ?>
-        <p class="print-date">Date: <?= date("F j, Y", strtotime("2025-06-30")) ?></p>
-        <p class="print-number">Index Number:<?= $s_user_name ?></p>
-        <p class="certificate-number">Certificate
-            ID:<?= $certificateId ?></p>
-        <p class="pv-number2">PV00253555</p>
-    </div>
-<?php
-    $count++;
-    if ($count == 10) {
+<?php if ($tableMode == 1) { ?>
+    <table border="1" cellspacing="0">
+        <tr>
+            <th style="padding: 10px;">Ceremony Number</th>
+            <th style="padding: 10px;">Name on Certificate</th>
+            <th style="padding: 10px;">Certificate ID</th>
+            <th style="padding: 10px;">Username</th>
+        </tr>
+    <?php } ?>
+    <?php
+    $count = 1;
+    foreach ($packageBookings as $booking) {
         // break;
+        $s_user_name = $booking['student_number'];
+        // $CourseCode = 1;
+
+        $batchStudents =  GetLmsStudents();
+        $studentDetailsArray = $batchStudents[$s_user_name];
+
+        $certificateId = ($courseCode == 1) ? $booking['certificate_id'] : $booking['advanced_id'];
+        $certificateInfo = CertificatePrintStatusByCertificateId($certificateId);
+        // var_dump($certificateInfo);
+
+        $PrintDate = date("Y-m-d H:i:s");
+        // $certificateEntryResult = EnterCertificateEntry($printDate, 1, $loggedUser, 'Workshop-Certificate', $s_user_name, $CourseCode);
+        // var_dump($certificateEntryResult);
+
+        // Include the qrlib file 
+        if (empty($certificateId)) {
+            $certificateInfo = CertificatePrintStatusByParentCourse($courseCode, 'Certificate', $s_user_name);
+            // var_dump($certificateInfo);
+            $certificateId = $certificateInfo[$s_user_name]['certificate_id'];
+            $batchCode = $certificateInfo[$s_user_name]['course_code'];
+        } else {
+            $batchCode = $certificateInfo[0]['course_code'];
+        }
+
+        $text = "https://pharmacollege.lk/result-view.php?CourseCode=" . $batchCode . "&LoggedUser=" . $s_user_name;
+        $ecc = 'L';
+        $pixel_Size = 10;
+        $frame_Size = 0;
+
+        // Generate the QR code image
+        ob_start();
+        QRcode::png($text, null, QR_ECLEVEL_L, 10, 0);
+        $image_data = ob_get_contents();
+        ob_end_clean();
+    ?>
+
+        <?php if ($tableMode == 1) { ?>
+
+            <tr>
+                <td style="padding: 10px;"><?= $booking['ceremony_number'] ?></td>
+                <td style="padding: 10px;"><?= $studentDetailsArray['name_on_certificate'] ?></td>
+                <td style="padding: 10px;"><?= $certificateId ?></td>
+                <td style="padding: 10px;"><?= $s_user_name ?></td>
+            </tr>
+        <?php } else { ?>
+            <div class="certificate-container d-none">
+                <div class="certificate">
+                    <h1 class="certificate-title">CERTIFICATE OF COMPLETION</h1>
+
+                    <p class="awarded-to">This certificate is awarded to</p>
+                    <div class="recipient-line"><?= $studentDetailsArray['name_on_certificate'] ?></div>
+
+                    <div class="recognition-text">
+                        in recognition of the<br>
+                        successful completion of the
+                    </div>
+                    <div class="course-line"><?= $courseName ?></div>
+
+                    <p class="offered-by">offered by</p>
+                    <div class="institution">Ceylon Pharma College</div>
+                    <div class="ceremony-details">
+                        The certificate award ceremony was held at the<br>
+                        BMICH, Colombo, Sri Lanka.
+                    </div>
+                </div>
+
+                <img class="qr-code" src="data:image/png;base64,<?= base64_encode($image_data) ?>">
+                <?php
+                if ($courseCode == 2) { ?>
+                    <img class="sign" src="sign.png" alt="">
+                    <div>
+                    </div>
+                    <p class="sign-dot">...................................................</p>
+                    <p class="director">Director</p>
+                <?php } ?>
+                <p class="print-date">Date: <?= date("F j, Y", strtotime("2025-06-30")) ?></p>
+                <p class="print-number">Index Number:<?= $s_user_name ?></p>
+                <p class="certificate-number">Certificate
+                    ID:<?= $certificateId ?></p>
+                <p class="pv-number2">PV00253555</p>
+            </div>
+        <?php } ?>
+
+        <?php
+        $count++;
+        if ($count == 10) {
+            // break;
+        }
+        // Add a page break after each certificate except the last one
+        if ($packageBookings !== end($packageBookings)) {
+            echo '<div style="page-break-after: always;"></div>';
+        }
     }
-    // Add a page break after each certificate except the last one
-    if ($packageBookings !== end($packageBookings)) {
-        echo '<div style="page-break-after: always;"></div>';
-    }
-}
-?>
+    if ($tableMode == 1) { ?>
+    </table>
+<?php }  ?>
