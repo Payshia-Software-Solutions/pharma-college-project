2d54c9e886e7fafba3b77fd22a57373550a8fbe0
thilinaruwan11 <ict20838@fot.sjp.ac.lk>
2025-08-01 23:53:27 +0530

    update server

--- server-chat/models/TicketMessage.php
+++ server-chat/models/TicketMessage.php
@@ -19,20 +19,57 @@ class TicketMessage
     }
     public function create($data)
     {
-        $stmt = $this->pdo->prepare("INSERT INTO ticket_messages (ticket_id, from_role, text, time, created_at) VALUES (?, ?, ?, ?, NOW())");
+        $stmt = $this->pdo->prepare("INSERT INTO ticket_messages (ticket_id, from_role, text, time, created_at, img_url) VALUES (?, ?, ?, ?, NOW(), ?)");
         $stmt->execute([
             $data['ticket_id'],
             $data['from_role'],
             $data['text'],
             $data['time'],
+            $data['img_url'] ?? null
         ]);
 
         // Get last inserted ID
         return $this->pdo->lastInsertId();
     }
 
+    public function update($id, $data)
+    {
+        $fields = [];
+        $params = [];
 
+        if (isset($data['ticket_id'])) {
+            $fields[] = "ticket_id = ?";
+            $params[] = $data['ticket_id'];
+        }
+        if (isset($data['from_role'])) {
+            $fields[] = "from_role = ?";
+            $params[] = $data['from_role'];
+        }
+        if (isset($data['text'])) {
+            $fields[] = "text = ?";
+            $params[] = $data['text'];
+        }
+        if (isset($data['time'])) {
+            $fields[] = "time = ?";
+            $params[] = $data['time'];
+        }
+        if (isset($data['img_url'])) {
+            $fields[] = "img_url = ?";
+            $params[] = $data['img_url'];
+        }
 
+        if (empty($fields)) {
+            return 0; // Nothing to update
+        }
+
+        $params[] = $id;
+
+        $sql = "UPDATE ticket_messages SET " . implode(", ", $fields) . " WHERE id = ?";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($params);
+
+        return $stmt->rowCount();
+    }
 
     public function delete($id)
     {
