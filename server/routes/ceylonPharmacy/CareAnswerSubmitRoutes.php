20de90caf380ac05cdb95cfc95833bd92ac81117
englishbasictobrilliance <englishbasictobrilliance@gmail.com>
2025-11-18 18:53:48 +0000

    reset routes of care answer CareAnswerSubmitRoutes

--- server/routes/ceylonPharmacy/CareAnswerSubmitRoutes.php
+++ server/routes/ceylonPharmacy/CareAnswerSubmitRoutes.php
@@ -1,13 +1,34 @@
 <?php
 // routes/ceylonPharmacy/CareAnswerSubmitRoutes.php
 
-require_once './controllers/ceylonPharmacy/CareAnswerSubmitController.php';
+require_once __DIR__ . '/../../controllers/ceylonPharmacy/CareAnswerSubmitController.php';
 
-// ... (other routes)
+// Assuming $pdo is available in the scope where this file is included.
+$careAnswerSubmitController = new CareAnswerSubmitController($pdo);
 
-// Add this new route
-$routes['POST /care-answer-submits/validate'] = function () use ($careAnswerSubmitController) {
-    $careAnswerSubmitController->validateAndCreate();
-};
-
-return $routes;
+return [
+    // Get all care answer submits
+    'GET /care-answer-submits$' => function () use ($careAnswerSubmitController) {
+        $careAnswerSubmitController->getAll();
+    },
+    // Get care answer submit by ID
+    'GET /care-answer-submits/(\d+)$' => function ($id) use ($careAnswerSubmitController) {
+        $careAnswerSubmitController->getById($id);
+    },
+    // Create new care answer submit
+    'POST /care-answer-submits$' => function () use ($careAnswerSubmitController) {
+        $careAnswerSubmitController->create();
+    },
+    // Validate and create a new submission
+    'POST /care-answer-submits/validate$' => function () use ($careAnswerSubmitController) {
+        $careAnswerSubmitController->validateAndCreate();
+    },
+    // Update care answer submit
+    'PUT /care-answer-submits/(\d+)$' => function ($id) use ($careAnswerSubmitController) {
+        $careAnswerSubmitController->update($id);
+    },
+    // Delete care answer submit
+    'DELETE /care-answer-submits/(\d+)$' => function ($id) use ($careAnswerSubmitController) {
+        $careAnswerSubmitController->delete($id);
+    },
+];
