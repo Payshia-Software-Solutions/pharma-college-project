9210f7286d91fa0b568471d3435441b09f3d61fd
englishbasictobrilliance <englishbasictobrilliance@gmail.com>
2025-11-27 10:14:39 +0000

    update

--- server/routes/ceylonPharmacy/CareCenterRecoveryRoutes.php
+++ server/routes/ceylonPharmacy/CareCenterRecoveryRoutes.php
@@ -12,19 +12,19 @@ return [
     'GET /care-center-recoveries/$' => function () use ($careCenterRecoveryController) {
         $careCenterRecoveryController->getAll();
     },
-    'GET /care-center-recoveries/(\d+)$' => function ($id) use ($careCenterRecoveryController) {
+    'GET /care-center-recoveries/(\d+)/$' => function ($id) use ($careCenterRecoveryController) {
         $careCenterRecoveryController->getById($id);
     },
-    'GET /care-center-recoveries/student/([^/]+)$' => function ($studentNumber) use ($careCenterRecoveryController) {
+    'GET /care-center-recoveries/student/([^/]+)/$' => function ($studentNumber) use ($careCenterRecoveryController) {
         $careCenterRecoveryController->getByStudentNumber($studentNumber);
     },
     'POST /care-center-recoveries/$' => function () use ($careCenterRecoveryController) {
         $careCenterRecoveryController->create();
     },
-    'PUT /care-center-recoveries/(\d+)$' => function ($id) use ($careCenterRecoveryController) {
+    'PUT /care-center-recoveries/(\d+)/$' => function ($id) use ($careCenterRecoveryController) {
         $careCenterRecoveryController->update($id);
     },
-    'DELETE /care-center-recoveries/(\d+)$' => function ($id) use ($careCenterRecoveryController) {
+    'DELETE /care-center-recoveries/(\d+)/$' => function ($id) use ($careCenterRecoveryController) {
         $careCenterRecoveryController->delete($id);
     },
 ];
