5cc83d420ee247d7d790d9689b1f08bd2839cbf7
englishbasictobrilliance <englishbasictobrilliance@gmail.com>
2026-01-06 18:46:46 +0000

    updat

--- server/models/SentenceBuilderSentence.php
+++ server/models/SentenceBuilderSentence.php
@@ -23,6 +23,13 @@ class SentenceBuilderSentence
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
+    public function getByLevelId($level_id)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM {$this->table} WHERE level_id = :level_id");
+        $stmt->execute(['level_id' => $level_id]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
     public function create($data)
     {
         $stmt = $this->pdo->prepare("INSERT INTO {$this->table} (level_id, correct_sentence, hint, translation) VALUES (:level_id, :correct_sentence, :hint, :translation)");
