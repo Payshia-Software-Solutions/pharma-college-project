f5bbcfae9b65da053e0e8e2e89b6c7a61fe88a86
englishbasictobrilliance <englishbasictobrilliance@gmail.com>
2026-01-17 15:59:34 +0000

    up

--- server/models/Convocation.php
+++ server/models/Convocation.php
@@ -27,29 +27,33 @@ class Convocation
 
     public function createConvocation($data)
     {
-        $stmt = $this->pdo->prepare("INSERT INTO `convocations` (`convocation_name`, `held_on`, `session_count`, `parent_seats`, `student_seats`, `created_by`, `created_at`) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt = $this->pdo->prepare("INSERT INTO `convocations` (`convocation_name`, `held_on`, `session_count`, `parent_seats`, `student_seats`, `session_2`, `created_by`, `created_at`, `accept_booking`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
         $stmt->execute([
             $data['convocation_name'],
             $data['held_on'],
             $data['session_count'],
             $data['parent_seats'],
             $data['student_seats'],
+            $data['session_2'],
             $data['created_by'],
-            $data['created_at']
+            $data['created_at'],
+            $data['accept_booking']
         ]);
     }
 
     public function updateConvocation($id, $data)
     {
-        $stmt = $this->pdo->prepare("UPDATE `convocations` SET `convocation_name` = ?, `held_on` = ?, `session_count` = ?, `parent_seats` = ?, `student_seats` = ?, `created_by` = ?, `created_at` = ? WHERE `id` = ?");
+        $stmt = $this->pdo->prepare("UPDATE `convocations` SET `convocation_name` = ?, `held_on` = ?, `session_count` = ?, `parent_seats` = ?, `student_seats` = ?, `session_2` = ?, `created_by` = ?, `created_at` = ?, `accept_booking` = ? WHERE `id` = ?");
         $stmt->execute([
             $data['convocation_name'],
             $data['held_on'],
             $data['session_count'],
             $data['parent_seats'],
             $data['student_seats'],
+            $data['session_2'],
             $data['created_by'],
             $data['created_at'],
+            $data['accept_booking'],
             $id
         ]);
     }
