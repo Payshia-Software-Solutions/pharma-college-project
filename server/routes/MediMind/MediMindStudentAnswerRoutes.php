89b1fda4f28038f48ef06f5e40f56766090275a4
thilinaruwan11 <ict20838@fot.sjp.ac.lk>
2026-03-20 19:56:22 +0530

    hjkhjk

--- server/routes/MediMind/MediMindStudentAnswerRoutes.php
+++ server/routes/MediMind/MediMindStudentAnswerRoutes.php
@@ -10,8 +10,8 @@ $controller = new MediMindStudentAnswerController($pdo);
 return [
     'GET /medi-mind-student-answers/' => [$controller, 'getAll'],
     'GET /medi-mind-student-answers/{id}/' => [$controller, 'getById'],
-    'GET /medi-mind-student-answers/student/{studentId}/' => [$controller, 'getByStudent'],
-    'GET /medi-mind-student-answers/stats/{studentId}/' => [$controller, 'getStatsByStudent'],
+    'GET /medi-mind-student-answers/student/{student_number}/' => [$controller, 'getByStudent'],
+    'GET /medi-mind-student-answers/stats/{student_number}/' => [$controller, 'getStatsByStudent'],
     'POST /medi-mind-student-answers/' => [$controller, 'create'],
     'PUT /medi-mind-student-answers/{id}/' => [$controller, 'update'],
     'DELETE /medi-mind-student-answers/{id}/' => [$controller, 'delete'],
